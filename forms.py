0ecdb11cd0e517f217070bf9ab9babc200af4f8c
Thomas John Twiton <twitth01@luther.edu>
2018-05-12 12:02:21 -0500

    two of three add forms done

--- forms.py
+++ forms.py
@@ -6,9 +6,15 @@ from wtforms import validators, ValidationError
 
 class AddPerson(FlaskForm):
     first_name = TextField("First name:", [validators.Required("Please enter a first name")])
-    middle_name = TextField("Middle name:")
+    middle_name = TextField("Middle name:",[validators.Required("Please enter a middle name")])
     last_name = TextField("Last name:", [validators.Required("Please enter a last name")])
-    nickname = TextField("Nickname:")
-    relation = TextField("Relationship:")
+    nickname = TextField("Nickname:", [validators.Required("Please enter a nickname (first name again will do)")])
+    relationship = TextField("Relationship:", [validators.Required("Please describe your relationship to this person")])
     submit = SubmitField("Add Person")
 
+class AddDate(FlaskForm):
+    person = SelectField("Important date for whom?",[validators.Required("Please pick a person")])
+    date = TextField("Date of the event (mm/dd/yyyy):",[validators.Required("Please enter a date in MM/DD/YYYY form")])
+    description = TextField("Description:",[validators.Required("Please enter a description of the date")])
+    submit = SubmitField("Add Date")
+    
--- giftapp.py
+++ giftapp.py
@@ -1,21 +1,56 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
-from forms import AddPerson
-import sqlite3
+from forms import AddPerson, AddDate
+import records
 
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = 'giftmaster'
 
 #Routes#
-@app.route('/add', methods=['GET', 'POST'])
-def addform():
+@app.route('/add_roster', methods=['GET', 'POST'])
+def addpeople():
     addform = AddPerson()
+
     if addform.validate_on_submit():
-        print(addform.first_name.data)
-        print(addform.last_name.data)
+        #print(addform.first_name.data)
+        db = records.Database('sqlite:///giftmaster.db')
+        db.query('insert into roster_test values ("'
+            +addform.first_name.data+'", "'
+            +addform.last_name.data+'", "'
+            +addform.middle_name.data+'", "'
+            +addform.nickname.data+'", "'
+            +addform.relationship.data+'", "'
+            +(addform.first_name.data + addform.last_name.data+ addform.middle_name.data + addform.nickname.data + addform.relationship.data)
+            + '")'
+        )
         return render_template("add_forms.html", form1 = addform, msg1 ='Person added to roster')
     return render_template("add_forms.html", form1 = addform, msg1='')
+
+@app.route('/add_date', methods=['GET', 'POST'])
+def adddate():
+    db = records.Database('sqlite:///giftmaster.db')
+    res=db.query('select first_name, last_name, id from roster_test')
+
+    dateform = AddDate()
+    dateform.person.choices = []
+
+    for name in res:
+        pid = name.id
+        display = name.first_name + " " + name.last_name
+        dateform.person.choices += [(pid, display)]
+
+    if dateform.validate_on_submit():
+        db.query('insert into date_test values ("'
+            +dateform.person.data + '", "'
+            +dateform.date.data + '", "'
+            +dateform.description.data + '")'
+        )
+        return render_template("add_forms.html", form1 = dateform, msg1='Date added to person')
+
+    return render_template("add_forms.html", form1 = dateform, msg1='')
+
+    
 
 if __name__=='__main__':
     app.run(debug=True)
